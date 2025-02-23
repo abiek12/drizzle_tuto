db8e6fea66e503d597096f078ec64ad8a371213f
Abhishek p p <abhishekkanichery@gmail.com>
2025-02-23 16:31:02 +0530

    insert users into db

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,27 @@ import { usersTable } from "./schema";
 
 const main = async () => {
     console.log('Server started');
+    await deleteUser();
     await insertUser();
     await fetchUsers();
 }
 
 const insertUser = async () => {
     try {
-        await db.insert(usersTable).values({
-            userName: "John Doe",
-            age: 30,
-            email: "john@mailinator.com",
-        })
+        const users = [
+            {
+                userName: "John Doe",
+                age: 30,
+                email: "john@mailinator.com",
+            },
+            {
+                userName: "Abhishek",
+                age: 25,
+                email: "=abhishek@mailinator.com",
+            },
+        ]
+        
+        await db.insert(usersTable).values(users).returning();
     } catch (error) {
         console.error(error);
         throw error;
@@ -22,8 +32,17 @@ const insertUser = async () => {
 
 const fetchUsers = async () => {
     try {
-        const users = await db.query.usersTable.findFirst();
+        const users = await db.query.usersTable.findMany();
         console.log(users);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+const deleteUser = async () => {
+    try {
+        await db.delete(usersTable)
     } catch (error) {
         console.error(error);
         throw error;
