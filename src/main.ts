e4f9fa226d3a8cd291f412ff445ec3c3d1eb050e
Abhishek p p <abhishekkanichery@gmail.com>
2025-03-01 11:58:21 +0530

    added categories

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { asc, eq, sql } from "drizzle-orm";
 import { db } from "./drizzle/db";
-import { categoryTable, postTable, userSettingsTable, usersTable } from "./schema";
+import { categoryTable, postCategoryTable, postTable, userSettingsTable, usersTable } from "./schema";
 import { QueryBuilder } from "drizzle-orm/pg-core";
 
 const main = async () => {
     console.log('Server started');
     await deleteUser();
     const user = await insertUser();
+    await createCategory();
     await insertPost(user.id);
     await fetchUsers();
 
@@ -38,7 +39,15 @@ const fetchUsers = async () => {
     try {
         const usersWithQuery = await db.query.usersTable.findMany({
             with: { 
-                posts: true, 
+                posts: {
+                    with: {
+                        postCategory: {
+                            with: {
+                                category: true
+                            }
+                        }
+                    }
+                }, 
                 userSettings: true 
             },
             columns: {createdAt: false, isDeleted: false, updatedAt: false},
@@ -56,8 +65,8 @@ const fetchUsers = async () => {
             .leftJoin(postTable, eq(postTable.authorId, usersTable.id))
             .groupBy(usersTable.id, userSettingsTable.id, postTable.id)
 
-        // console.dir(usersWithQuery, {depth: null});
-        console.log("Users with SQL:", usersWithSql);
+        console.dir(usersWithQuery, {depth: null});
+        // console.log("Users with SQL:", usersWithSql);
     } catch (error) {
         console.error(error);
         throw error;
@@ -86,18 +95,50 @@ const insertUserSettings = async (userId: number) => {
 
 const insertPost = async (userId: number) => {
     try {
-        await db.insert(postTable).values([
+        const postsData = [
             {
                 title: "My first blog!",
                 content: "This is my first blog",
-                authorId: userId
+                authorId: userId,
             },
             {
-                title: "My first blog!",
-                content: "This is my first blog",
-                authorId: userId
+                title: "My second blog!",
+                content: "This is my second blog",
+                authorId: userId,
             }
-        ])
+        ]
+        let newPost = await db.insert(postTable).values(postsData).returning();
+        newPost.map(async (post)=> {
+            await db.insert(postCategoryTable).values({
+                postId: post.id,
+                categoryId: 1
+            })
+        })
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+const createCategory = async () => {
+    try {
+        const categoriesData = [
+            { name: "Technology" },
+            { name: "Science" },
+            { name: "Art" },
+            { name: "Music" },
+            { name: "Lifestyle" },
+            { name: "Fashion" },
+            { name: "Food" },
+            { name: "Travel" },
+        ]
+        for (let i=0; i<categoriesData.length; i++) {
+            const categoryExists = await db.query.categoryTable.findFirst({
+                where: ((category, fnc)=> fnc.ilike(category.name, categoriesData[i].name))
+            });
+            if (categoryExists) continue;
+            await db.insert(categoryTable).values(categoriesData[i]);
+        }
     } catch (error) {
         console.error(error);
         throw error;
